ac7157c1ce0db9bb04ebf64011b6dec5b35ac71f
Golpezas <antonio.hernandezmm@gmail.com>
2025-01-04 00:31:33 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+#comentario de una sola linea
+"""Comentarios de varias lineas"""
+'''Este tambien es un comentario de varias lineas'''
+
+#Esto es un comentario Hola mundo
+
+print("Hello, World!")
+print('Hello, World!')
+
+print(type("Hello, World!"))
+print(type(7))
+print(type(2.71))
+print(type(True))
+print(type(1 + 2j))
